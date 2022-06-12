a4cdced457df90923bdc0e9d7322633859900020
Alexander <chubanov.alexander@yandex.ru>
2022-06-13 00:59:23 +0300

    Обновлена функция для редактирования текста и срока выполнения задачи.

--- edit_task.py
+++ edit_task.py
@@ -3,8 +3,10 @@
 Открываем нужный файл.
 Выводим в консоль пронумерованное построчно содержимое файла.
 Пользователь указывает номер задачи, которую нужно отредактировать.
-Возвращаем пользователю нужную строку: дату и название задачи.
-Пользователь вводит новые значения
+Возвращаем пользователю нужную строку: название задачи и срок выполнения.
+Пользователь вводит новые значения.
+Уточняем, будем ли изменять срок выполнения.
+При необходимости принимаем новый срок.
 Записывем новые данные по индексу в список.
 Возвращаем список.
 '''
@@ -20,10 +22,14 @@ def edit_task(file_now, file_past):
     with open(file_flag[0], 'r') as read_data:
         list_task = read_data.readlines()
     new_list = list(enumerate(list_task))
-    print(f'№ \t задача')
+    print(f'№ \t задача \t срок')
     for i in new_list:
-        print(f'{i[0]} \t {i[1]}')
+        print(f'{i[0]} \t {i[1]} \t {i[2]}')
     what_is_task = int(input("Введите номер задачи: "))
     edit_line = (new_list[what_is_task])[1]
-    list_task[what_is_task] = input(f"Измените задачу {edit_line}") + "\n"
+    list_task[what_is_task] = input(f"Измените текст задачи {edit_line}") + "\n"
+    change_date = input("Хотите изменить срок выполнения задачи? Ведите 'да' или 'нет': ")
+    if change_date != 'нет' or change_date != 'no':
+        edit_line = (new_list[what_is_task])[2]
+        list_task[what_is_task] = input(f"Введите новый срок выполнения задачи {edit_line}") + "\n"
     return list_task
